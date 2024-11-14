cb9eb2c86d33a3f6b22173a2289d5630013afc2f
Reinvdb <rein.vdb@hotmail.com>
2024-11-14 18:29:03 +0100

    final commit

--- loan_default_prediction/src/constants.py
+++ loan_default_prediction/src/constants.py
@@ -1,4 +1,2 @@
-import os
-
-PATH_VISUALIZATION = f"{os.getcwd}/visualizations/data_understanding"
-PATH_RAW_DATA = f"{os.getwcd}/data/raw/applicant_loan_data.csv"
+PATH_VIS = "./loan_default_prediction/visualizations/data_understanding"
+PATH_RAW_DATA = "./loan_default_prediction/data/raw/applicant_loan_data.csv"
